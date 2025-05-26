bb2b1a17743fdfd70489121af6ce3a097e52c89f
89VAnh <vietanha9a2@gmail.com>
2025-05-26 19:50:05 +0700

    style: format db.ts for consistency and readability

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,13 @@
-import { Pool } from 'pg'
+import { Pool } from "pg";
 
 const pool = new Pool({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DATABASE,
-    password: process.env.POSTGRES_PASSWORD,
-    port: parseInt(process.env.POSTGRES_PORT || '5432'),
-})
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: parseInt(process.env.POSTGRES_PORT || "5432"),
+  ssl:
+    process.env.POSTGRES_SSL === "true" ? { rejectUnauthorized: false } : false,
+});
 
-export default pool 
+export default pool;
